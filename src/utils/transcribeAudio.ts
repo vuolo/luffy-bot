6182a4359d48b699bb5d4e3186838a50f5f45810
Michael Vuolo <michaelv@tilli.pro>
2025-01-12 17:56:45 -0500

    refactor: improve formatting and add fallback message for transcription failure in transcribeAudio function

--- src/utils/transcribeAudio.ts
+++ src/utils/transcribeAudio.ts
@@ -1,36 +1,44 @@
 import { CommandInteraction } from "discord.js";
 
 // transcribes audio data using google cloud speech-to-text client
-export default async function transcribeAudio(inputAudio: Buffer, client: any, interaction: CommandInteraction) {
-    const config = {
-        encoding: "LINEAR16",
-        sampleRateHertz: 16000,
-        languageCode: "en-us",
-        speech_contexts: [
-            {
-                phrases: ["play", "skip", "pause", "resume", "disconnect"],
-            },
-        ],
-    };
+export default async function transcribeAudio(
+  inputAudio: Buffer,
+  client: any,
+  interaction: CommandInteraction
+) {
+  const config = {
+    encoding: "LINEAR16",
+    sampleRateHertz: 16000,
+    languageCode: "en-us",
+    speech_contexts: [
+      {
+        phrases: ["play", "skip", "pause", "resume", "disconnect"],
+      },
+    ],
+  };
 
-    const audio = {
-        content: inputAudio,
-    };
+  const audio = {
+    content: inputAudio,
+  };
 
-    const request = {
-        audio: audio,
-        config: config,
-    };
+  const request = {
+    audio: audio,
+    config: config,
+  };
 
-    const [response]: any = await client.recognize(request);
-    const transcription = response.results
-        .map((result: any) => result.alternatives[0].transcript)
-        .join("\n");
-    console.log("Billed time: ", response.totalBilledTime);
-    console.log("Transcription: ", transcription);
-    if (transcription) await interaction.channel!.send(
-        `**Transcription:** \`${transcription || "<nothing detected>"}\``
+  const [response]: any = await client.recognize(request);
+  const transcription = response.results
+    .map((result: any) => result.alternatives[0].transcript)
+    .join("\n");
+  console.log("Billed time: ", response.totalBilledTime);
+  console.log("Transcription: ", transcription);
+  if (transcription)
+    await interaction.channel!.send(
+      `**Transcription:** \`${transcription || "<nothing detected>"}\``
     );
+  else {
+    await interaction.channel!.send("couldn't hear u bitch");
+  }
 
-    return transcription;
+  return transcription;
 }
